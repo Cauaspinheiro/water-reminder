98b6d410b198f34bca5c92a1b2dde16c2ac1b3ae
Cauã Pinheiro <cauaspinheiro@gmail.com>
2021-05-06 12:47:02 -0300

    feat: remove custom topbar

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -6,7 +6,6 @@ import AnimatedNumber from '../components/AnimatedNumber'
 import HomeFooterCard from '../components/HomeFooterCard'
 import LinearProgressIndicator from '../components/LinearProgressIndicator'
 import Sidebar from '../components/Sidebar'
-import Topbar from '../components/Topbar'
 import WaterDrop from '../components/WaterDrop'
 import { useWaterProgressContext } from '../context/water-progress'
 import useHomeTitle from '../hooks/useHomeTitle'
@@ -40,69 +39,64 @@ const Home: React.FC = () => {
   if (isServer) return <div />
 
   return (
-    <div className="w-screen h-screen ">
-      <Topbar />
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      transition={{ duration: 1 }}
+      variants={variants}
+      className="flex items-center justify-between h-full px-10 pt-4 pb-12 lg:px-16"
+    >
+      <Sidebar />
 
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        transition={{ duration: 1 }}
-        variants={variants}
-        className="flex items-center justify-between h-full px-10 pt-4 pb-12 lg:px-16"
-      >
-        <Sidebar />
+      <div className="flex flex-col items-center justify-between w-full h-full xl:ml-24">
+        <motion.h1 className="mt-6 text-4xl font-semibold text-title">
+          {homeTitle}
+        </motion.h1>
 
-        <div className="flex flex-col items-center justify-between w-full h-full xl:ml-24">
-          <motion.h1 className="mt-6 text-4xl font-semibold text-title">
-            {homeTitle}
-          </motion.h1>
+        <div className="relative flex items-center justify-center w-full water-drop">
+          <WaterDrop />
 
-          <div className="relative flex items-center justify-center w-full water-drop">
-            <WaterDrop />
+          <AnimatedNumber
+            componentProps={{
+              className:
+                'absolute text-6xl font-semibold text-title font-poppins top-2/3'
+            }}
+            initialValue={percent}
+            to={percent}
+            textPattern="{number}%"
+          >
+            {motion.h1}
+          </AnimatedNumber>
+        </div>
+
+        <motion.footer
+          initial="hidden"
+          animate="visible"
+          variants={footerVariants}
+          transition={{ duration: 1.4 }}
+          className="flex items-center justify-around w-full h-44 gap-x-4 lg:gap-x-16"
+        >
+          <HomeFooterCard title="Seu nível de água" variant="hover">
+            <LinearProgressIndicator percent={percent} />
 
             <AnimatedNumber
-              componentProps={{
-                className:
-                  'absolute text-6xl font-semibold text-title font-poppins top-2/3'
-              }}
-              initialValue={percent}
-              to={percent}
-              textPattern="{number}%"
+              textPattern={`{number}ml / ${progress.meta}ml`}
+              initialValue={progress.achieved}
+              to={progress.achieved}
+              componentProps={{ className: 'text-xl text-content' }}
             >
-              {motion.h1}
+              {motion.span}
             </AnimatedNumber>
-          </div>
+          </HomeFooterCard>
 
-          <motion.footer
-            initial="hidden"
-            animate="visible"
-            variants={footerVariants}
-            transition={{ duration: 1.4 }}
-            className="flex items-center justify-around w-full h-44 gap-x-4 lg:gap-x-16"
-          >
-            <HomeFooterCard title="Seu nível de água" variant="hover">
-              <LinearProgressIndicator percent={percent} />
-
-              <AnimatedNumber
-                textPattern={`{number}ml / ${progress.meta}ml`}
-                initialValue={progress.achieved}
-                to={progress.achieved}
-                componentProps={{ className: 'text-xl text-content' }}
-              >
-                {motion.span}
-              </AnimatedNumber>
-            </HomeFooterCard>
-
-            <HomeFooterCard title="Você irá beber água em" variant="infinite">
-              <span className="text-5xl md:text-6xl text-content">
-                {formattedRemainingTime.minutes}:
-                {formattedRemainingTime.seconds}
-              </span>
-            </HomeFooterCard>
-          </motion.footer>
-        </div>
-      </motion.div>
-    </div>
+          <HomeFooterCard title="Você irá beber água em" variant="infinite">
+            <span className="text-5xl md:text-6xl text-content">
+              {formattedRemainingTime.minutes}:{formattedRemainingTime.seconds}
+            </span>
+          </HomeFooterCard>
+        </motion.footer>
+      </div>
+    </motion.div>
   )
 }
 
